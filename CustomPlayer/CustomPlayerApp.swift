4dec43f2fd99fca86090fba7616c63c734f5239e
Vlad Blotenko <blosenkovlad@gmail.com>
2024-05-08 13:44:14 +0300

    Initial Commit

---
+++ CustomPlayer/CustomPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPlayerApp.swift
+//  CustomPlayer
+//
+//  Created by Blotenko on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
